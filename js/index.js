ae89ac5372c92f1b11746383dfb5e93ee2b74ea8
lesthy900 <lesthy900@gmail.com>
2023-05-08 03:50:37 +0200

    condi

---
+++ js/index.js
@@ -0,0 +1,213 @@
+/* ej 1 */
+
+/* let num1 = 4;
+let num2 = 5;
+
+if (num1 > num2) {
+  console.log(`El número ${num1} es mayor que el número ${num2}.`);
+} else if (num2 > num1) {
+  console.log(`El número ${num2} es mayor que el número ${num1}.`);
+} else {
+  console.log("Los números son iguales.");
+} */
+
+/* ej2 */
+
+let numUno = 5;
+let numDos = 5;
+
+if (numUno === numDos) {
+    
+  console.log("Los números  iguales.");
+} else {
+  console.log("Los números son diferentes.");
+}
+
+/* ej3 */
+
+let numeroG1 = 30;
+let numeroG2 = 40;
+
+if (numeroG1 > numeroG2){
+
+    console.log(`el numeor ${numeroG1} es mas grande que ${numeroG2}`);
+    
+}else{
+    console.log(`el numero ${numeroG2} es mas grande que ${numeroG1}`);
+}
+
+/* ej4 */
+
+/* let num11 = parseInt(prompt("Ingrese el primer número:"));
+let num22 = parseInt(prompt("Ingrese el segundo número:"));
+let num33 = parseInt(prompt("Ingrese el tercer número:"));
+
+if (num1 <= num22 && num11 <= num33) {
+  console.log(`El número más chico es ${num11}.`);
+} else if (num22 <= num11 && num2 <= num33) {
+  console.log(`El número más chico es ${num22}.`);
+} else {
+  console.log(`El número más chico es ${num33}.`);
+}
+ */
+/* ej5 */
+const persona1 = {
+    nombre: "lesty",
+    edad: 28,
+    altura: 1.88,
+  };
+  
+  const persona2 = {
+    nombre: "kimberly",
+    edad: 30,
+    altura: 1.80,
+  };
+  
+  if (persona1.altura > persona2.altura) {
+    console.log(`${persona1.nombre} es más alto/a que ${persona2.nombre}.`);
+  } else if (persona2.altura > persona1.altura) {
+    console.log(`${persona2.nombre} es más alto/a que ${persona1.nombre}.`);
+  } else {
+    console.log(`${persona1.nombre} y ${persona2.nombre} tienen la misma altura.`);
+  }
+  
+  if (persona1.edad > persona2.edad) {
+    console.log(`${persona1.nombre} es mayor que ${persona2.nombre}.`);
+  } else if (persona2.edad > persona1.edad) {
+    console.log(`${persona2.nombre} es mayor que ${persona1.nombre}.`);
+  } else {
+    console.log(`${persona1.nombre} y ${persona2.nombre} tienen la misma edad.`);
+  }
+ 
+  
+
+
+  /* je6 */
+ /*  const nombre = prompt("Ingresa tu nombre:");
+  const edad = parseInt(prompt("Ingresa tu edad:"));
+  const altura = parseFloat(prompt("Ingresa tu altura (en metros):"));
+  const vision = parseFloat(prompt("Ingresa tu agudeza visual (de 1 a 10):"));
+
+  if (edad >= 18 && altura > 1.5 && vision >= 8) {
+  console.log(`${nombre}, estás capacitado/a para conducir.`);
+  } else {
+  console.log(`${nombre}, no estás capacitado/a para conducir.`);
+  } */
+  
+
+
+
+/*   let edad1 =parseInt(prompt("ingrese su eda"))
+
+  if(edad1 >= 0 && edad1 <=12){
+    console.log(`es infante`)
+  }else if(edad1 >12 && edad1 <= 18){
+    console.log(`es adolescente`)
+
+}else if( edad1 > 18 && edad1 <= 45 ){
+    console.log(`es joven mayor`)
+
+  }else if(edad1 > 45 && edad1 <= 70){
+     console.log(`es adulto mayor `)
+  }else{
+    console.log(`¿en realidad es esa su edad?`)
+    alert(`¿en realidad es esa su edad?`)
+  } */
+
+
+
+
+
+/*   const jueogDemano1 =prompt("ingrese piedra, papel, o tijeras").toLowerCase();
+  const jueogDemano  =prompt("ingrese piedra, papel, o tijeras").toLowerCase();
+let piedra;
+let papel;
+let tijeras
+
+
+
+if(jueogDemano1 === jueogDemano){
+    console.log(`empate`)
+}else if( 
+ (jueogDemano1 === "piedra" && jueogDemano === "tijeras") ||
+ (jueogDemano1 === "papel" && jueogDemano === "piedra") ||
+ (jueogDemano1 === "tijeras" && jueogDemano === "papel")){
+    console.log(`gana jugador 1`)
+}else if( 
+ (jueogDemano === "papel" && jueogDemano1 === "piedra") ||
+ (jueogDemano === "tijeras" && jueogDemano1 === "papel") ||
+ (jueogDemano === "piedra" && jueogDemano1 === "tijeras")){
+    console.log(`gana jugador 2 `)
+}else{
+    console.log(`alguien hace trampa`)
+}
+ */
+
+
+
+
+
+/* const color = prompt("Ingresa un color:");
+
+switch (color.toLowerCase()) {
+  case "blanco":
+  case "negro":
+    console.log("Falta de color");
+    break;
+  case "verde":
+    console.log("El color de la naturaleza");
+    break;
+  case "amarillo":
+    console.log("El color del sol");
+    break;
+  case "rojo":
+    console.log("El color del fuego");
+    break;
+  case "marron":
+    console.log("El color de la tierra");
+    break;
+          default: console.log("Excelente elección, no lo teníamos pensado");
+    break;
+}
+ */
+
+
+
+
+    
+  /*   const operacion = prompt("Ingresa la operación a realizar (suma, resta, multiplicacion, division):");
+    let num1 = parseFloat(prompt("Ingresa el primer número:"));
+    let num2 = parseFloat(prompt("Ingresa el segundo número:"));
+    
+
+
+    switch (operacion) {
+        case "suma":
+          console.log(`el resultado de sumar ${num1} y ${num2} es ${num1 + num2}`)
+            break;
+        case "resta":
+          console.log(`el resultado de restar ${num1} y ${num2} es ${num1 - num2}`)
+            break;
+        case "multipicacion":
+            console.log(`el resultado de multiplicar ${num1} y ${num2} es ${num1 * num2}`)
+            break;
+        case "division":
+            if(num2 === 0){
+                console.log(`ERROR no se pueder dividir ${num1} entre ${num2} cambia el divisor `)
+            }else {
+                
+                console.log(`el rsultado de dividir ${num1} y ${num2} es ${num1 / num2} `)
+            }
+             break;
+           
+        default:console.log("Operación no válida");
+        break;
+    } */
+    
+  
+
+
+
+
+
+
